15c183296f10399e5b83e3d777bfe5190a54e692
de-scientist <gitaumark502@gmail.com>
2025-11-19 15:41:43 +0300

    install react-router-dom@6

--- src/main.tsx
+++ src/main.tsx
@@ -1,20 +1,23 @@
-import { useEffect ,StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import './index.css'
+import React from 'react';
+import { createRoot } from 'react-dom/client';
+import { BrowserRouter } from 'react-router-dom';
+import App from './App';
+import './index.css';
+
+// 3rd party styles
 import 'bootstrap/dist/css/bootstrap.min.css';
-import 'bootstrap/dist/js/bootstrap.bundle.min.js';
 import 'aos/dist/aos.css';
-import AOS from 'aos';
+import '@fortawesome/fontawesome-free/css/all.min.css';
 
-import App from './App.tsx'
+// 3rd party JS (Bootstrap & AOS will be initialized in App)
+import 'bootstrap/dist/js/bootstrap.bundle.min.js';
 
-useEffect(() => {
-  AOS.init();
-}, []);
-
-
-createRoot(document.getElementById('root')!).render(
-  <StrictMode>
-    <App />
-  </StrictMode>,
-)
+const container = document.getElementById('root')!;
+const root = createRoot(container);
+root.render(
+  <React.StrictMode>
+    <BrowserRouter>
+      <App />
+    </BrowserRouter>
+  </React.StrictMode>
+);
