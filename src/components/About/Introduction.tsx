f5d14beef6d57c7fcb5b11c58f035712bb51ee61
de-scientist <gitaumark502@gmail.com>
2025-11-20 18:15:24 +0300

    update the introduction

--- src/components/About/Introduction.tsx
+++ src/components/About/Introduction.tsx
@@ -9,16 +9,17 @@ const Introduction: React.FC = () => {
           <div className="col-lg-7 mb-4 mb-lg-0" data-aos="fade-right" data-aos-delay="100">
             <h2 className="section-title">Who We Are</h2>
             <p className="lead">
-              The Murang'a University of Technology Christian Union (MUTCU)
-              is a lively, student-led religious organization...
+             The Murang'a University of Technology Christian Union (MUTCU) is a lively, student-led religious organization operating within the university.
+                         We are a non-denominational, non-political and non-profit making society committed to building in spiritual growth, nurturing leadership, and impacting society with the Gospel of Jesus Christ.
             </p>
 
             <p>
               As a proud member of{" "}
                 <a href="https://focuskenya.org/" target="_blank" rel="noreferrer noopener"
                   className="text-decoration-none mutcu-link-orange">
-                FOCUS-KENYA
-              </a>, MUTCU benefits from a national network...
+                Fellowship of Christian Unions - Kenya (FOCUS-KENYA)
+              </a>, MUTCU benefits from a national network of support and accountability. This affiliation ensures our activities are theologically sound, well-guided,
+                         and contribute to broader Christian initiatives. Our identity and all functions are meticulously guided by a comprehensive constitution and detailed operational policies for each ministry.
             </p>
           </div>
 
@@ -26,7 +27,7 @@ const Introduction: React.FC = () => {
             <img
               src="/assets/images/Full Logo.png"
               className="img-fluid rounded-3 shadow-lg"
-              alt="MUTCU"
+              alt="MUTCU Community"
             />
           </div>
 
